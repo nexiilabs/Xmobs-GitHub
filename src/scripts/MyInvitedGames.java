120e78d5706153953ced05965a38f5db93b3ea90
Admin <Admin@ranjeethv.nexiilabs.net>
2014-11-15 15:30:49 +0530

    Complete & Working Code MyInvitedGame Code Uploaded by Santhosh

--- src/scripts/MyInvitedGames.java
+++ src/scripts/MyInvitedGames.java
@@ -54,15 +54,13 @@ public class MyInvitedGames {
 	    
 	    driver = new SelendroidDriver(caps);
 	    
-	  //Signin Button 
+	   //Signin Button Validation
 	    if(!driver.findElements(By.id("btn_sign_in")).isEmpty()) {
 		  	System.out.println("\n"+"Validating Signin Button");
 	    	driver.findElement(By.id("btn_sign_in")).click();
 		   }
 	    
-	    
-	    
-	    
+	   
 	    //Validating my invited games from home page
 	    System.out.println("Validating my invited games button in the home page");
 	    sleep(3);
@@ -75,6 +73,231 @@ public class MyInvitedGames {
 	    waitforelement(20, By.id("action_bar_title"));
 	    System.out.println("The current page is : " +driver.findElement(By.id("action_bar_title")).getText());
 	    
+//=================================================================================================================================================
+
+//Validating Current Games
+	    
+	    //Getting the Current Games list and printing Thier Names.
+	    List<WebElement> CurrentGamesList_0 = driver.findElement(By.xpath("//FrameLayout[@id='content']")).findElements(By.id("lv_view_scheduled_games"));
+	    // System.out.println(CurrentGamesList_0.size());
+	    Thread.sleep(5000);
+	    
+	    List<WebElement> CurrentGamesList_1 = new ArrayList<WebElement>();
+	    CurrentGamesList_1 = CurrentGamesList_0.get(0).findElements(By.id("tv_item_joined_game_name"));   
+	    System.out.println("Number of games in Current Games List are : " +  CurrentGamesList_1.size());
+	    System.out.println("Name of the Games in Waiting Games :");
+	    
+	    for(int g=0;g<CurrentGamesList_1.size();g++){
+	    	System.out.println(CurrentGamesList_1.get(g).getText());
+	    }
+	    
+	    Random r = new Random();
+	    int CurrentGame = r.nextInt(CurrentGamesList_1.size());
+	    System.out.println("\n"+"Random Game Number selected is: " +CurrentGame+" ,  Randomly selected game name is: "+ CurrentGamesList_1.get(CurrentGame).getText());
+	   // String Name = CurrentGamesList_1.get(CurrentGame).getText();
+	   // System.out.println(Name);
+	    Thread.sleep(6000);
+	    CurrentGamesList_1.get(CurrentGame).click();
+	    Thread.sleep(6000);
+	   
+	    System.out.println("\n"+"The Current Page is "+driver.findElement(By.id("action_bar_title")).getText()+ " Page");
+	    //Handling Notes
+	    System.out.println("\n"+"First Note Name : " + driver.findElement(By.id("tv_current_note_name")).getText());
+	    System.out.println("First Note Desc : " +driver.findElement(By.id("tv_current_note_desc")).getText());
+	    
+	    Thread.sleep(6000);
+	    System.out.println("\n"+"Clicking on My Location Button");
+	    driver.findElement(By.xpath("//View[@name='My Location']")).click();;
+	    
+	    Thread.sleep(6000);
+	    System.out.println("Clicking on View Change Button");
+	    driver.findElement(By.id("iv_change_view")).click();
+	   
+	    Thread.sleep(6000);
+	    System.out.println("Clicking on Zoom In Button");
+	    driver.findElement(By.xpath("//View[@name='Zoom in']")).click();
+	    
+	    Thread.sleep(6000);
+	    System.out.println("Clicking on Zoom Out Button");
+	    driver.findElement(By.xpath("//View[@name='Zoom out']")).click();
+	    Thread.sleep(5000);
+	    driver.navigate().back();
+	    Thread.sleep(10000);
+	    
+	    
+// Validating Waiting Games
+	    System.out.println("\n"+"Validating Waiting Games"+"\n");
+	    driver.findElement(By.xpath("//TextView[@value='Waiting Games']")).click();;
+	    Thread.sleep(5000);
+	    //Waiting Games
+	    List<WebElement> WaitingGamesList_0 = driver.findElement(By.xpath("//FrameLayout[@id='content']")).findElements(By.id("lv_view_scheduled_games"));
+	    //System.out.println(WaitingGamesList_0.size());
+	    Thread.sleep(10000);
+	    List<WebElement> WaitingGames_1 = new ArrayList<WebElement>();
+	    WaitingGames_1 = WaitingGamesList_0.get(1).findElements(By.id("tv_item_joined_game_name"));   
+	    System.out.println("Number of games in waiting games List : " +  WaitingGames_1.size());
+	    System.out.println("Name of the Games in Waiting Games :");
+	    
+	    for(int g=0;g<WaitingGames_1.size();g++){
+	    	System.out.println(WaitingGames_1.get(g).getText());
+	    }
+	    
+	    Thread.sleep(6000);
+	    int WaitedGame = r.nextInt(WaitingGames_1.size());
+	    //System.out.println("Randomly selected game Name is : "+);
+	    System.out.println("Randomly Selected Game Number : " +WaitedGame+" ,  Randomly Selected Game Name : "+ WaitingGames_1.get(WaitedGame).getText());
+	    String Name = WaitingGames_1.get(WaitedGame).getText();
+	    //System.out.println(Name);
+	    Thread.sleep(6000);
+	    WaitingGames_1.get(WaitedGame).click();
+	    Thread.sleep(6000);
+	    System.out.println("\n"+"The Current Page is "+driver.findElement(By.id("action_bar_title")).getText()+ " Page");
+	    System.out.println("\n"+"Clicking on Not Ready Button ");
+	    driver.findElement(By.id("bt_reject_game")).click();
+	    Thread.sleep(10000);
+	    
+	    //Navigation To HomePage
+	    System.out.println("Title of Navigated Page: " + driver.findElement(By.id("action_bar_title")).getText());
+	    
+	    //Reverting back to Waiting games
+	    Thread.sleep(3000);
+	    driver.findElement(By.linkText("My Invited Games")).click();
+	    Thread.sleep(3000);
+	    driver.findElement(By.xpath("//TextView[@value='Waiting Games']")).click();;
+	    Thread.sleep(10000);
+	    
+	    for(int v=0;v<WaitingGames_1.size();v++){
+		Thread.sleep(10000);
+		   
+	//======================================================================================================================================================
+		List<WebElement> WaitingGames3 = driver.findElement(By.xpath("//FrameLayout[@id='content']")).findElements(By.id("lv_view_scheduled_games"));
+		Thread.sleep(5000);
+		List<WebElement> WaitingGames4 = new ArrayList<WebElement>();
+		WaitingGames4 = WaitingGames3.get(1).findElements(By.id("tv_item_joined_game_name"));   
+	//========================================================================================================================================================  
+		   
+		if(WaitingGames4.get(v).getText().equalsIgnoreCase(Name));{
+		Thread.sleep(3000);
+		System.out.println("\n"+"Reverting back to "+driver.findElement(By.id("action_bar_title")).getText()+ " Page");
+		WaitingGames4.get(v).click();
+		Thread.sleep(3000);
+		System.out.println("\n"+"Clicking on Not Ready Button ");
+		driver.findElement(By.id("bt_join_game")).click();
+		Thread.sleep(5000);
+		}
+	    }
+		//Navigation To HomePage
+		System.out.println("Title of Navigated Page: " + driver.findElement(By.id("action_bar_title")).getText());
+	    
+	    
+//Validating Completed games
+		
+		Thread.sleep(3000);
+	    driver.findElement(By.linkText("My Invited Games")).click();
+	    Thread.sleep(3000);
+		System.out.println("\n"+"Validating Current Games"+"\n");
+	    driver.findElement(By.xpath("//TextView[@value='Completed Games']")).click();;
+	    Thread.sleep(5000);
+	    
+	    List<WebElement> CompletedGamesList_0 = driver.findElement(By.xpath("//FrameLayout[@id='content']")).findElements(By.id("lv_view_scheduled_games"));
+	    //System.out.println(CompletedGamesList_0.size());
+	    Thread.sleep(10000);
+	    List<WebElement> CompletedGamesList_1 = new ArrayList<WebElement>();
+	    CompletedGamesList_1 = CompletedGamesList_0.get(1).findElements(By.id("tv_item_joined_game_name"));   
+	    System.out.println("\n"+"Number of games in Completed games List : " +  CompletedGamesList_1.size());
+	    System.out.println("Name of the Games in Completed Games :");
+	    
+	    for(int g=0;g<CompletedGamesList_1.size();g++){
+	    	System.out.println(CompletedGamesList_1.get(g).getText());
+	    }
+	    
+	    Thread.sleep(6000);
+	    int CompletedGame = r.nextInt(CompletedGamesList_1.size());
+	    System.out.println("\n"+"Randomly Selected Game Number : " +CompletedGame+" ,  Randomly Selected Game Name : "+ CompletedGamesList_1.get(CompletedGame).getText());
+	    String Name1 = CompletedGamesList_1.get(CompletedGame).getText();
+	    //System.out.println(Name);
+	    Thread.sleep(6000);
+	    CompletedGamesList_1.get(CompletedGame).click();
+	    Thread.sleep(6000);
+	    
+	    System.out.println("\n"+"The Current Page is "+driver.findElement(By.id("action_bar_title")).getText()+ " Page");
+	    List<WebElement> GameStatsticsNamesList = driver.findElement(By.id("lv_players_completed_game")).findElements(By.id("tv_completed_player_name"));
+	   //System.out.println(GameStatsticsNamesList.size());
+	    
+	    List<WebElement> GameStatsticsRanksList = driver.findElements(By.id("tv_player_rank"));
+	   // System.out.println(GameStatsticsRanksList.size());
+	    
+	    System.out.println("Printing the Names & Their respective Ranks");
+	    for(int GSN=0; GSN<GameStatsticsNamesList.size();GSN++){
+	    	
+	    	System.out.println(GameStatsticsNamesList.get(GSN).getText()+" : "+GameStatsticsRanksList.get(GSN).getText());
+	    	    	
+	    }
+	  //Navigation To HomePage
+	  		System.out.println("Title of Navigated Page: " + driver.findElement(By.id("action_bar_title")).getText());
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	  /*  Thread.sleep(3000);
+	   //   driver.findElement(By.linkText("My Invited Games")).click();
+	   //   Thread.sleep(3000);
+	     // driver.findElement(By.xpath("//TextView[@value='Waiting Games']")).click();;
+	   driver.findElement(By.xpath("//TextView[@value='current Games']")).click();;
+	      Thread.sleep(10000);
+	    
+	      List<WebElement> WaitingGamesList_0 = driver.findElement(By.xpath("//FrameLayout[@id='content']")).findElements(By.id("lv_view_scheduled_games"));
+	       System.out.println(WaitingGamesList_0.size());
+	    Thread.sleep(10000);
+	    List<WebElement> WaitingGames_1 = new ArrayList<WebElement>();
+	    WaitingGames_1 = WaitingGamesList_0.get(0).findElements(By.id("tv_item_joined_game_name"));   
+	    System.out.println("Number of games in waiting games List : " +  WaitingGames_1.size());
+	    System.out.println("Name of the Games in Waiting Games :");
+	    
+	    for(int g=0;g<WaitingGames_1.size();g++){
+	    	System.out.println(WaitingGames_1.get(g).getText());
+	    }
+	    
+	    
+	  //  tv_current_note_name
+	    
+	    tv_current_note_name
+	    
+	    driver.findElement(By.xpath("//View[@name='My Location']"));
+	    
+	    iv_change_view
+	    Zoom in
+//==================================================================================================================================================	    
 	    // validating current games when having multiple games waiting
 	/*    
 	    driver.findElement(By.linkText("Current Games")).click();
@@ -91,6 +314,7 @@ public class MyInvitedGames {
 	    	sleep(2);
 	    
 	    }
+	   
 	    Random cu = new Random();
 	    int a = cu.nextInt(CuG.size());
 	    System.out.println("Random Game selected is for test is : " +a+"\nThe Game name is : " +CuG.get(a).findElement(By.id("tv_item_joined_game_name")).getText());
@@ -147,7 +371,7 @@ public class MyInvitedGames {
 	   System.out.println("The game name from Xmobs pae is : " +driver.findElement(By.id("tv_current_note_name")).getText());
 	   System.out.println("The Note Description is : " +driver.findElement(By.id("tv_current_note_desc")).getText());
 	   */
-	    sleep(20);
+	    sleep(1000000);
 	    
 	  }
 }
