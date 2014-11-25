6bd1f3276f116213d8b927fd995eecea195c04e0
ajaykovuri <akajaykovuri@gmail.com>
2014-11-25 12:31:42 +0530

    Updating "My Invited Games" and "AppIconNavigation" from the inputs of Ameer

--- src/scripts/AppIconNavigation.java
+++ src/scripts/AppIconNavigation.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 import testUtils.Helper;
 
 public class AppIconNavigation extends Helper{
-  @Test
+  @Test 
    public void startSelendroidServer() throws Exception {
 			
 			if (selendroidServer != null) {
@@ -34,32 +34,25 @@ public class AppIconNavigation extends Helper{
 		    	sleep(3);
 		    	waitToLoadForElement(20, By.id("btn_join_game"));
 		    	System.out.println("Clicking on " +driver.findElement(By.id("btn_join_game")).getText());
-		    	driver.findElement(By.id("btn_join_game")).click();
-		   
-		    	
-		    	
+		    	driver.findElement(By.id("btn_join_game")).click();		    	
 		    
-	//Validating Invited Games page
+//Validating Invited Games page
 		    
 		    	sleep(3);
 		    	waitToLoadForElement(20, By.id("action_bar_title"));
 		    	System.out.println("The current page is : " +driver.findElement(By.id("action_bar_title")).getText());
-	  
-	  
-	  String game_page = driver.findElement(By.id("action_bar_title")).getText();
-	   System.out.println("The current page is : " +driver.findElement(By.id("action_bar_title")).getText());
-	   driver.findElement(By.id("action_bar_title")).click();
-	   sleep(5);
-	   waitToLoadForElement(20, By.id("action_bar_title"));
-	   System.out.println("After clicking now the page is : " +driver.findElement(By.id("action_bar_title")).getText());
-	   String home_page = driver.findElement(By.id("action_bar_title")).getText();
-	   
-	   if(game_page.equals(home_page)){
-		   System.out.println("Home page is not navigated");
-	   }
-	   else{
-		   
-	   System.out.println("Home page navigated successfully");
+		    String game_page = driver.findElement(By.id("action_bar_title")).getText();
+	  	  		System.out.println("The current page is : " +driver.findElement(By.id("action_bar_title")).getText());
+				driver.findElement(By.id("action_bar_title")).click();
+				sleep(5);
+				waitToLoadForElement(20, By.id("action_bar_title"));
+				System.out.println("After clicking now the page is : " +driver.findElement(By.id("action_bar_title")).getText());
+			String home_page = driver.findElement(By.id("action_bar_title")).getText();
+			if(game_page.equals(home_page)){
+				System.out.println("Home page is not navigated");
+			}
+			else{ 
+				System.out.println("Home page navigated successfully");
 	   }
 	  
   } 
